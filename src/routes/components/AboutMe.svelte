afb7e70eb88faad50fd98732effac91c947cbe28
Sudip Halder <dev@sudip.me>
2024-09-13 12:59:48 +0530

    updated profile image and about me paragraph

--- src/routes/components/AboutMe.svelte
+++ src/routes/components/AboutMe.svelte
@@ -5,30 +5,42 @@
     id="about-me"
     class="max-w-[1400px] md:w-[75vw] flex flex-col flex-col-reverse md:flex-row md:justify-between items-center my-10 px-6 z-1"
 >
-    <p class="md:pr-20 font-helvetica text-justify text-white text-md md:text-lg lg:text-xl">
-        <span class="text-lg md:text-xl"> Hello there! I'm Sudip. </span>
-        <br /><br />
-        I'm currently pursuing an undergraduate degree in Computer Science at the
-        Indian Institute of Information Technology, Sri City.
-        <br /><br />
-        One of my favorite aspects of being a computer science student is participating
-        in weekly coding contests. These challenges allow me to apply my problem-solving
-        skills, algorithmic thinking, and programming prowess to real-world scenarios.
-        <br /><br />
-        Machine learning is a domain that truly excites me. I am actively investing
-        time and effort into understanding the intricacies of ML algorithms, techniques,
-        and their practical applications.
+    <p
+        class="md:pr-20 font-helvetica text-justify text-gray-300 text-md md:text-lg lg:text-xl"
+    >
+        <span class="leading-relaxed"> Hello there! I'm Sudip. </span>
+        <br />
+
+        <span class="leading-relaxed hidden lg:inline-block">
+            I'm currently pursuing an undergraduate degree in Computer Science
+            at the <br /> Indian Institute of Information Technology, Sri City.
+        </span>
+
+        <span class="leading-relaxed lg:hidden">
+            I'm currently pursuing an undergraduate degree in Computer Science
+            at the Indian Institute of Information Technology, Sri City.
+        </span>
+
+        <br />
+        <span class="leading-relaxed">
+            I like to work on Machine Learning algorithms and train deep neural
+            nets.
+        </span>
+
+        <br />
+        <br />
+        Feel free to reach out to me on X. I’m mostly active there.
     </p>
 
-        <img
-            src="/images/profile.jpeg"
-            alt="sudip halder"
-            class="w-[240px] rounded-2xl md:rounded-2xl mb-8 md:mb-0"
-        />
+    <img
+        src="/images/profile.jpeg"
+        alt="sudip halder"
+        class="w-[240px] rounded-full mb-8 md:mb-0"
+    />
 </div>
 
 <style lang="postcss">
-        :global(p) {
+    :global(p) {
         @apply font-light text-base text-justify leading-7;
         word-spacing: -0.1em;
     }
--- src/routes/mail/+page.svelte
+++ src/routes/mail/+page.svelte
@@ -1,144 +1,145 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { goto } from "$app/navigation";
-  import NavBar from "../components/NavBar.svelte";
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+    import NavBar from "../components/NavBar.svelte";
+    import { ApiBaseUrl } from "$lib/config";
 
-  let name = "";
-  let email = "";
-  let message = "";
-  let color = "transparent";
-  let responseMessage = "Sending message . . .";
+    let name = "";
+    let email = "";
+    let message = "";
+    let color = "transparent";
+    let responseMessage = "Sending message . . .";
 
-  const ApiBaseUrl = "YOUR_API_BASE_URL_HERE"; // Replace with your actual API base URL
+    async function handleSubmit(event: Event) {
+        event.preventDefault();
+        color = "green";
 
-  async function handleSubmit(event: Event) {
-    event.preventDefault();
-    color = "green";
+        const formData = { name, email, message };
 
-    const formData = { name, email, message };
+        try {
+            const response = await fetch(ApiBaseUrl + "send-mail", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData),
+            });
 
-    try {
-      const response = await fetch(ApiBaseUrl + "send-mail", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        if (data.error_message) {
-          responseMessage = data.error_message;
-          color = "red";
-        } else {
-          responseMessage = data.message;
-          color = "green";
-          name = email = message = "";
+            if (response.ok) {
+                const data = await response.json();
+                if (data.error_message) {
+                    responseMessage = data.error_message;
+                    color = "red";
+                } else {
+                    responseMessage = data.message;
+                    color = "green";
+                    name = email = message = "";
+                }
+            } else {
+                responseMessage = "Something went wrong!";
+                color = "red";
+            }
+        } catch (error) {
+            responseMessage = "Something went wrong!";
+            color = "red";
         }
-      } else {
-        responseMessage = "Something went wrong!";
-        color = "red";
-      }
-    } catch (error) {
-      responseMessage = "Something went wrong!";
-      color = "red";
     }
-  }
 
-  onMount(() => {
-    document.title = "Send Mail to Sudip Halder";
-  });
+    onMount(() => {
+        document.title = "Send Mail to Sudip Halder";
+    });
 </script>
 
 <svelte:head>
-  <meta
-    name="description"
-    content="Sudip Halder is a Software Developer and a Machine Learning expert. Read his blogs on Machine Learning and AI, and learn more about his projects and experiences."
-  />
+    <meta
+        name="description"
+        content="Sudip Halder is a Software Developer and a Machine Learning expert. Read his blogs on Machine Learning and AI, and learn more about his projects and experiences."
+    />
 </svelte:head>
 
 <div id="navbar-spacer" class="h-[60px] bg-primary"></div>
 <NavBar />
 
 <div
-  class="w-screen h-[calc(100vh-60px)] min-h-[600px] flex justify-center items-center font-inter"
+    class="w-screen h-[calc(100vh-60px)] min-h-[600px] flex justify-center items-center font-inter"
 >
-  <form
-    on:submit={handleSubmit}
-    class="max-w-[1400px] w-full h-4/5 flex flex-col items-center"
-    id="contact-form"
-    name="mail-form"
-  >
-    <div
-      class="w-[90%] lg:w-2/5 h-4/5 flex flex-col justify-around items-center"
-      id="contact-form-input-container"
+    <form
+        on:submit={handleSubmit}
+        class="max-w-[1400px] w-full h-4/5 flex flex-col items-center"
+        id="contact-form"
+        name="mail-form"
     >
-      <input
-        class="form-input w-full h-[50px] pl-5 my-2.5 text-white text-lg bg-transparent border border-[#555] rounded-[20px]"
-        type="text"
-        bind:value={name}
-        placeholder="Name"
-        maxlength={20}
-        required
-      />
+        <div
+            class="w-[90%] lg:w-2/5 h-4/5 flex flex-col justify-around items-center"
+            id="contact-form-input-container"
+        >
+            <input
+                class="form-input w-full h-[50px] pl-5 my-2.5 text-white text-lg bg-transparent border border-[#555] rounded-[20px]"
+                type="text"
+                bind:value={name}
+                placeholder="Name"
+                maxlength={20}
+                required
+            />
 
-      <input
-        class="form-input w-full h-[50px] pl-5 my-2.5 text-white text-lg bg-transparent border border-[#555] rounded-[20px]"
-        type="email"
-        bind:value={email}
-        placeholder="Email Id"
-        maxlength={50}
-        required
-      />
+            <input
+                class="form-input w-full h-[50px] pl-5 my-2.5 text-white text-lg bg-transparent border border-[#555] rounded-[20px]"
+                type="email"
+                bind:value={email}
+                placeholder="Email Id"
+                maxlength={50}
+                required
+            />
 
-      <textarea
-        class="form-input w-full min-h-[50%] max-h-full p-5 my-2.5 text-white text-base bg-transparent border border-[#555] rounded-[20px] resize-none"
-        bind:value={message}
-        placeholder="Message . . ."
-        maxlength={1000}
-        required
-      ></textarea>
+            <textarea
+                class="form-input w-full min-h-[50%] max-h-full p-5 my-2.5 text-white text-base bg-transparent border border-[#555] rounded-[20px] resize-none"
+                bind:value={message}
+                placeholder="Message . . ."
+                maxlength={1000}
+                required
+            ></textarea>
 
-      <p
-        class="m-0 text-center text-xl font-bold"
-        id="response-msg"
-        style="color: {color}"
-      >
-        {responseMessage}
-      </p>
-    </div>
+            <p
+                class="m-0 text-center text-xl font-bold"
+                id="response-msg"
+                style="color: {color}"
+            >
+                {responseMessage}
+            </p>
+        </div>
 
-    <div class="h-[10%] lg:h-1/5 max-w-screen flex justify-center items-center">
-      <button
-        type="button"
-        on:click={() => goto("/")}
-        class="w-[150px] lg:w-[200px] h-10 lg:h-12 m-2 lg:m-[5px] text-[#ddd] text-xl lg:text-2xl bg-[#252525] rounded-[20px]"
-        id="back-button"
-      >
-        Cancel
-      </button>
-      <button
-        type="submit"
-        class="w-[150px] lg:w-[200px] h-10 lg:h-12 m-2 lg:m-[5px] text-white text-xl lg:text-2xl bg-[#252525] rounded-[20px]"
-        id="submit-btn"
-      >
-        Submit
-      </button>
-    </div>
-  </form>
+        <div
+            class="h-[10%] lg:h-1/5 max-w-screen flex justify-center items-center"
+        >
+            <button
+                type="button"
+                on:click={() => goto("/")}
+                class="w-[150px] lg:w-[200px] h-10 lg:h-12 m-2 lg:m-[5px] text-[#ddd] text-xl lg:text-2xl bg-[#252525] rounded-[20px]"
+                id="back-button"
+            >
+                Cancel
+            </button>
+            <button
+                type="submit"
+                class="w-[150px] lg:w-[200px] h-10 lg:h-12 m-2 lg:m-[5px] text-white text-xl lg:text-2xl bg-[#252525] rounded-[20px]"
+                id="submit-btn"
+            >
+                Submit
+            </button>
+        </div>
+    </form>
 </div>
 
 <style>
-  .form-input:focus {
-    outline: none;
-    border: 2px solid rgba(41, 151, 255, 1);
-  }
+    .form-input:focus {
+        outline: none;
+        border: 2px solid rgba(41, 151, 255, 1);
+    }
 
-  .form-input::placeholder {
-    padding-left: 10px;
-    color: #aaa;
-  }
+    .form-input::placeholder {
+        padding-left: 10px;
+        color: #aaa;
+    }
 
-  .form-input:focus::placeholder {
-    color: transparent;
-  }
+    .form-input:focus::placeholder {
+        color: transparent;
+    }
 </style>
