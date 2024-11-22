861ef2554c6177990c5ce0a673b364b0d540530e
Sudip Halder <87203402+sudipme@users.noreply.github.com>
2024-11-22 19:27:40 +0530

    change alignment of featured blogs row
    Signed-off-by: Sudip Halder <87203402+sudipme@users.noreply.github.com>

--- src/routes/components/FeaturedArticles.svelte
+++ src/routes/components/FeaturedArticles.svelte
@@ -25,23 +25,25 @@
     });
 </script>
 
-
-<div  class="max-w-[1400px] w-[90%] md:w-[75%] no-underline mt-10">
+<div class="max-w-[1400px] w-[90%] md:w-[75%] no-underline mt-10">
     <h1 class="text-3xl text-[#f5f5f7] font-raleway font-normal mb-4">
-        Articles <a href="{base}/articles" class="text-lg ml-4 text-link">( View All )</a>
+        Articles <a href="{base}/articles" class="text-lg ml-4 text-link"
+            >( View All )</a
+        >
     </h1>
 </div>
 
 <div
-    class="max-w-[1400px] w-[90%] md:w-[75%] flex flex-col justify-between items-center overflow-hidden"
+    class="max-w-[1400px] w-[90%] md:w-[75%] flex flex-col justify-between items-start overflow-hidden"
 >
     <!-- featured blogs shadow, shadow-[0_100px_200px_10px_#fff] -->
 
     {#each featuredBlogs as blog (blog.time_stamp)}
-        <a href="{base}/articles/{blog.blog_id}" class="no-underline cursor-pointer">
-            <div
-                class="my-2.5 overflow-hidden cursor-pointer"
-            >
+        <a
+            href="{base}/articles/{blog.blog_id}"
+            class="no-underline cursor-pointer"
+        >
+            <div class="my-2.5 overflow-hidden cursor-pointer">
                 <h2
                     class="my-1 text-left font-sohne text-2xl md:text-xl font-normal text-white hover:text-gray-300"
                 >
@@ -55,5 +57,4 @@
             </div>
         </a>
     {/each}
-
 </div>
